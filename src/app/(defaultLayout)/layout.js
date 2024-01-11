9b5b96d6cfb2a1d82078c5e56b1b409a68556c71
RidoyMia <ridoymia153159hero@gmail.com>
2024-01-11 21:42:02 +0600

    footer section completed

--- src/app/(defaultLayout)/layout.js
+++ src/app/(defaultLayout)/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/Shared/Header/Header'
+import Footer from '@/components/Pages/Footer/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Header></Header>
         {children}
+        <Footer></Footer>
       </body>
     </html>
   )
